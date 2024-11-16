0f6d4925b1818f215a6051973e27cede352eba8f
cua843 <s26815132@gmail.com>
2024-11-16 22:44:41 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://cua843.github.io',
+    site: 'https://cua843.github.io/cua843-site',
   })
