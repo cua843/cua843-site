5669004662b46ce07710ad0502cf4983001bbb8f
cua843 <s26815132@gmail.com>
2024-11-17 23:16:55 +0800

    update

--- .astro/astro/content.d.ts
+++ .astro/astro/content.d.ts
@@ -176,28 +176,28 @@ declare module 'astro:content' {
   slug: "project-1";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "project-2.md": {
 	id: "project-2.md";
   slug: "project-2";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "project-3.md": {
 	id: "project-3.md";
   slug: "project-3";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 "project-4.md": {
 	id: "project-4.md";
   slug: "project-4";
   body: string;
   collection: "projects";
-  data: InferEntrySchema<"projects">
+  data: any
 } & { render(): Render[".md"] };
 };
 
@@ -209,5 +209,5 @@ declare module 'astro:content' {
 
 	type AnyEntryMap = ContentEntryMap & DataEntryMap;
 
-	export type ContentConfig = typeof import("../../src/content/config.js");
+	export type ContentConfig = never;
 }
